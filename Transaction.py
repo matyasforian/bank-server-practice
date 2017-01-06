78554ca03bc54ad3dcfa4519c2167cea95b0393c
Laszlo Molnar <laszlo.molnar@codecool.com>
2017-01-06 15:35:58 +0100

    Transaction class with init

---
+++ Transaction.py
@@ -0,0 +1,7 @@
+class Transaction:
+
+    def __init__(self, sender, receiver, amount, date_time):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.date_time = date_time
