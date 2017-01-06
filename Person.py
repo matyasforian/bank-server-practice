155ec24b76091cd6d13bfb14900b3046b91192b0
Laszlo Molnar <laszlo.molnar@codecool.com>
2017-01-06 15:39:59 +0100

    Person class with basic init

---
+++ Person.py
@@ -0,0 +1,8 @@
+class Person:
+
+    def __init__(self, id, name, birth_date, password):
+        self.id = id
+        self.name = name
+        self.birth_date = birth_date
+        self.password = password
+        self.bank_accounts = []
